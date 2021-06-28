9facede245b8ea148cbbdbc839b1c04d85589c4f
acagreen17 <acagreen17@gmail.com>
2021-06-28 13:31:14 +0100

    calculate time since second dose

--- analysis/data_process.R
+++ analysis/data_process.R
@@ -15,7 +15,7 @@
 library('tidyverse')
 library('lubridate')
 
-## Custom function
+## Custom functions
 fct_case_when <- function(...) {
   # uses dplyr::case_when but converts the output to a factor,
   # with factors ordered as they appear in the case_when's  ... argument
@@ -23,6 +23,16 @@ fct_case_when <- function(...) {
   levels <- sapply(args[-1], function(f) f[[3]])  # extract RHS of formula
   levels <- levels[!is.na(levels)]
   factor(dplyr::case_when(...), levels=levels)
+}
+
+tte <- function(origin_date, event_date, censor_date, na.censor=FALSE){
+  # returns time-to-event date or time to censor date, which is earlier
+  
+  if (na.censor)
+    time <- event_date-origin_date
+  else
+    time <- pmin(event_date-origin_date, censor_date-origin_date, na.rm=TRUE)
+  as.numeric(time)
 }
 
 ## Output processed data to rds
@@ -162,9 +172,20 @@ data_processed <- data_extract %>%
       region == "West Midlands" ~ "West Midlands",
       region == "Yorkshire and The Humber" ~ "Yorkshire and the Humber",
       #TRUE ~ "Unknown",
-      TRUE ~ NA_character_)
+      TRUE ~ NA_character_),
+    
+    # Censoring
+    censor_date = pmin(death_date, 
+                       dereg_date, 
+                       as.Date(Sys.Date(), format = "%Y-%m-%d"), 
+                       na.rm=TRUE),
+    
+    # Time since second dose
+    follow_up_time = tte(covid_vax_2_date,
+                         as.Date(Sys.Date(), format = "%Y-%m-%d"),
+                         censor_date)
     ) %>%
-  select(patient_id, covid_vax_1_date, covid_vax_2_date, dereg_date,
+  select(patient_id, covid_vax_1_date, covid_vax_2_date, follow_up_time,
          covid_hospital_admission, covid_hospitalisation_critical_care, covid_death, death,
          care_home, care_home_65plus, shielded, age, ageband, hscworker, immunosuppression, 
          first_positive_test_date, latest_positive_test_date, 
